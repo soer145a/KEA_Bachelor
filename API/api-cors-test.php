80944a882d45c4a1446515ff66efc6625845d26f
soer145a <soren.remboll@gmail.com>
2021-04-23 09:50:14 +0200

    login page

--- API/api-cors-test.php
+++ API/api-cors-test.php
@@ -2,14 +2,12 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-
-
+$db = "purplescout";
 // Create connection
 $conn = new mysqli($servername, $username, $password);
-
+$conn->query("INSERT * INTO customers WHERE 'name = 'John'");
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 echo "Connected successfully";
-?>
---
+++ login.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PurpleScout Login</title>
+</head>
+<body>
+<h1>Login</h1>
+    <form method="post">
+        <label><p>Enter Email:</p>
+            <input type="email" name="customer_email" id="">
+        </label>
+        <label><p>Enter Password:</p>
+            <input type="password" name="customer_password" id="">
+        </label>
+        <br>
+        <button type="submit">Login</button>
+    </form>
+</body>
+</html>
