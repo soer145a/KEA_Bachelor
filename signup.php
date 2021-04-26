f019c290004a9a7d747bb4c89de84506e2708526
Lev-pt <daniel-beck@hotmail.com>
2021-04-26 13:31:20 +0200

    Frontend validation implemented on sign-up form

--- signup.php
+++ signup.php
@@ -1,46 +1,46 @@
 <?php
-session_start();
-$inputFields = 0;
-$errorMsg = "";
-$errorEmail = "";
-$errorCvr = "";
-$errorPass = "";
-foreach ($_POST as $key) {
-    if ($key != "") {
-        $inputFields++;
-    }
-}
-if ($inputFields != 7) {
-    $errorMsg = "Fuck you fill out the form";
-} else {
-    //echo "THANKS FOR YOUR DATA FUCK FACE";
-    include("DB_Connection/connection.php");
-    $sEmail = strtolower($conn->real_escape_string($_POST['input_email']));
-    $sCVR = $conn->real_escape_string($_POST['input_company_cvr']);
-    $sPasswordInit = $_POST['input_password_init'];
-    $sPasswordConfirm = $_POST['input_password_confirm'];
-    $stmt = $conn->prepare("SELECT customer_email FROM customers WHERE customer_email = ?");
-    $stmt->bind_param("s", $sEmail);
-    $stmt->execute();
-    $data = $stmt->get_result();
-    $convertedData = $data->fetch_object();
-    //echo $convertedData->customer_email;
-    if (isset($convertedData->customer_email)) {
-        $errorEmail = "<p style='color:red'>Your email was stolen sucker</p>";
-    }
-    $stmt = $conn->prepare("SELECT customer_cvr FROM customers WHERE customer_cvr = ?");
-    $stmt->bind_param("s", $sCVR);
-    $stmt->execute();
-    $data = $stmt->get_result();
-    $convertedData = $data->fetch_object();
-    //echo $convertedData->customer_cvr;
-    if (isset($convertedData->customer_cvr)) {
-        $errorCvr = "<p style='color:red'>Your company was already fo shizzle registered</p>";
-    }
-    if ($sPasswordInit !== $sPasswordConfirm) {
-        $errorPass = "<p style='color:red'> Your big dumb head can't spell for shitz</p>";
-    }
-}
+// session_start();
+// $inputFields = 0;
+// $errorMsg = "";
+// $errorEmail = "";
+// $errorCvr = "";
+// $errorPass = "";
+// foreach ($_POST as $key) {
+//     if ($key != "") {
+//         $inputFields++;
+//     }
+// }
+// if ($inputFields != 7) {
+//     $errorMsg = "Fuck you fill out the form";
+// } else {
+//     //echo "THANKS FOR YOUR DATA FUCK FACE";
+//     include("DB_Connection/connection.php");
+//     $sEmail = strtolower($conn->real_escape_string($_POST['input_email']));
+//     $sCVR = $conn->real_escape_string($_POST['input_company_cvr']);
+//     $sPasswordInit = $_POST['input_password_init'];
+//     $sPasswordConfirm = $_POST['input_password_confirm'];
+//     $stmt = $conn->prepare("SELECT customer_email FROM customers WHERE customer_email = ?");
+//     $stmt->bind_param("s", $sEmail);
+//     $stmt->execute();
+//     $data = $stmt->get_result();
+//     $convertedData = $data->fetch_object();
+//     //echo $convertedData->customer_email;
+//     if (isset($convertedData->customer_email)) {
+//         $errorEmail = "<p style='color:red'>Your email was stolen sucker</p>";
+//     }
+//     $stmt = $conn->prepare("SELECT customer_cvr FROM customers WHERE customer_cvr = ?");
+//     $stmt->bind_param("s", $sCVR);
+//     $stmt->execute();
+//     $data = $stmt->get_result();
+//     $convertedData = $data->fetch_object();
+//     //echo $convertedData->customer_cvr;
+//     if (isset($convertedData->customer_cvr)) {
+//         $errorCvr = "<p style='color:red'>Your company was already fo shizzle registered</p>";
+//     }
+//     if ($sPasswordInit !== $sPasswordConfirm) {
+//         $errorPass = "<p style='color:red'> Your big dumb head can't spell for shitz</p>";
+//     }
+// }
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -49,46 +49,49 @@ if ($inputFields != 7) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/app.css">
     <title>Sign Up</title>
 </head>
 
 <body>
     <h1>Sign Up</h1>
-    <form method="post">
+    <form class="form" method="post" onsubmit="return inputValidate()">
         <label>
             <p>Contact - First Name:</p>
-            <input type="text" name="input_first_name" value="SAM">
+            <input class="form__input" oninput="inputValidate()" data-validate="string" type="text" name="input_first_name" placeholder="John">
         </label>
         <label>
             <p>Contact - Last Name:</p>
-            <input type="text" name="input_last_name" value="UEL">
+            <input class="form__input" oninput="inputValidate()" data-validate="string" type="text" name="input_last_name" placeholder="Doe">
         </label>
         <label>
             <p>Contact - Email:</p>
-            <input type="email" name="input_email" value="sam@uel.dk">
-            <?= $errorEmail ?>
+            <input class="form__input" oninput="inputValidate()" data-validate="email" type="email" name="input_email" placeholder="example@email.com">
+            <!-- <?= $errorEmail ?> -->
         </label>
         <label>
             <p>Company - Name:</p>
-            <input type="text" name="input_company_name" value="SAMS's bar">
+            <input class="form__input" oninput="inputValidate()" data-validate="string" type="text" name="input_company_name" placeholder="JohnDoe A/S">
         </label>
         <label>
-            <p>Company - CVR: ( Skriv kun talene )</p>
-            <input type="text" name="input_company_cvr" value="12399">
-            <?= $errorCvr ?>
+            <p>Company - CVR:</p>
+            <input class="form__input" oninput="inputValidate()" data-validate="cvr" type="text" name="input_company_cvr" placeholder="12345678">
+            <!-- <?= $errorCvr ?> -->
         </label>
         <label>
             <p>Password: ( No special characters )</p>
-            <input type="password" name="input_password_init" value="XXBAJER">
+            <input class="form__input" oninput="inputValidate()" data-validate="password" type="password" name="input_password_init" placeholder="MyStr0ng.PW-example">
         </label>
         <label>
             <p>Confirm Password:</p>
-            <input type="password" name="input_password_confirm" value="XXBAJER">
-            <?= $errorPass ?>
+            <input class="form__input" oninput="inputValidate()" data-validate="password" type="password" name="input_password_confirm" placeholder="MyStr0ng.PW-example">
+            <!--  <?= $errorPass ?> -->
         </label>
-        <input type="submit" value="Sign up!">
-        <p><?= $errorMsg ?></p>
+        <button class="form__btn" type="submit">Sign up</button>
+        <!--       <p><?= $errorMsg ?></p> -->
     </form>
 </body>
+
+<script src="js/app.js"></script>
 
 </html>
