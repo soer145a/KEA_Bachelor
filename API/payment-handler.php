dba4a536c3cf3064e46a5912041d763896991fa2
soer145a <soren.remboll@gmail.com>
2021-04-28 11:35:24 +0200

    User data gathered into a single object

--- API/payment-handler.php
+++ API/payment-handler.php
@@ -3,5 +3,14 @@ session_start();
 //echo "starting the payment";
 include("../DB_Connection/connection.php");
 
-$sql = "INSERT INTO customers VALUES (customer_first_name, customer_last_name, customer_company_name, customer_email, customer_password, customer_cvr) VALUES ('John', 'Doe', 'john@example.com')";
-$result = $conn->query();
+//$sql = "INSERT INTO customers VALUES (customer_first_name, customer_last_name, customer_company_name, customer_email, customer_password, customer_cvr) VALUES ('John', 'Doe', 'john@example.com')";
+//$result = $conn->query();
+foreach ($_SESSION['tempUserData'] as $key) {
+    echo "$key <br>";
+}
+
+/* $stmt = $conn->prepare("INSERT INTO customers VALUES (customer_first_name, customer_last_name, customer_company_name, customer_email, customer_password, customer_cvr) VALUES (?,?,?,?,?,?)");
+$stmt->bind_param("s", $sEmail);
+$stmt->execute();
+$data = $stmt->get_result();
+$convertedData = $data->fetch_object(); */
