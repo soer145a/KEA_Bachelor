c2e99270cbec91251b740df731365c614896bee8
Mikkel Madsen <mikk837k@stud.kea.dk>
2021-05-25 12:52:21 +0200

    sections added plus styling

--- js/helper.js
+++ js/helper.js
@@ -1,5 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
     updateSliderButton();
+    toggleDropdown();
+    dropdownSelector();
 });
 
 function updateSliderButton() {
@@ -18,6 +20,50 @@ function updateSliderButton() {
             });
         });
     }
+}
+
+function toggleDropdown() {
+    if (document.querySelector(".dropdown") !== null) {
+        let aDropdownBtnElements =
+            document.querySelectorAll(".dropdown__button");
+        let aDropdownlistElements = document.querySelectorAll(
+            ".dropdown-list-container"
+        );
+        aDropdownBtnElements.forEach((button) => {
+            console.log(button);
+            button.addEventListener("click", () => {
+                aDropdownlistElements.forEach((list) => {
+                    if (button.dataset.btnid === list.dataset.listid) {
+                        list.classList.toggle(
+                            "dropdown-list-container--hidden"
+                        );
+                    }
+                });
+            });
+        });
+    }
+}
+
+function dropdownSelector() {
+    let aDropdownItems = "";
+    if (document.querySelector(".dropdown__list-item") !== null) {
+        aDropdownItems = document.querySelectorAll(".dropdown__list-item");
+        for (let i = 0; i < aDropdownItems.length; i++) {
+            aDropdownItems[i].addEventListener("click", () => {
+                clickedItem(aDropdownItems[i]);
+            });
+        }
+    }
+}
+
+function clickedItem(item) {
+    if (localStorage.length < 0) {
+        console.log(item);
+    }
+    localStorage.setItem("chosenItem", item.dataset.value);
+    let selectedElement = item;
+    let dropdownButton = document.querySelector(".dropdown__button");
+    dropdownButton.textContent = selectedElement.textContent;
 }
 
 // function scrollToItem(itemPosition, numItems, scroller) {
