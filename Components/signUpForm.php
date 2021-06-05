098f6c13101ee098712d19dbf0dcdcab8bee5821
Lev-pt <daniel-beck@hotmail.com>
2021-06-05 20:43:38 +0200

    added sign up form comp

---
+++ Components/signUpForm.php
@@ -0,0 +1,147 @@
+<?php
+//The sign up form component
+function signUpFormComp($bLoginStatus, $nTotalPrice)
+{
+    $sSignUpFormHtmlComp =
+        "
+        <form action='api/payment-handler.php' method='POST' class='account-details'>
+            <h2 class='section-header'>Account details</h2>
+            <label for='account-details__name'
+                >Company Name</label
+            >
+            <input
+                id='account-details__name'
+                type='text'
+                name='companyName'
+                data-validate='string'
+                required
+                oninput='inputValidate(); togglePaypalButton($bLoginStatus, $nTotalPrice);'
+            />
+            <label for='account-details__cvr'
+                >Company CVR nr.</label
+            >
+            <input
+                id='account-details__cvr'
+                type='text'
+                name='companyCvr'
+                data-validate='cvr'
+                required
+                oninput='inputValidate(); togglePaypalButton($bLoginStatus, $nTotalPrice);'
+            />
+            <div class='account-details__contact'>
+                <h4 class='section-subheader contact__header'>Contact Person</h4>
+                <div class='contact__wrapper'>
+                    <label for='contact__firstname'>Firstname</label>
+                    <input
+                        id='contact__firstname'
+                        type='text'
+                        name='customerFirstName'
+                        data-validate='string'
+                        required
+                        oninput='inputValidate(); togglePaypalButton($bLoginStatus, $nTotalPrice);'
+                    />
+                </div>
+                <div class='contact__wrapper'>
+                    <label for='contact__lastname'>Lastname</label>
+                    <input
+                        id='contact__lastname'
+                        type='text'
+                        name='customerLastName'
+                        data-validate='string'
+                        required
+                        oninput='inputValidate(); togglePaypalButton($bLoginStatus, $nTotalPrice);'
+                    />
+                </div>
+            </div>
+            
+            <label for='account-details__mail'>Phone Number</label>
+            <input
+                id='account-details__mail'
+                type='text'
+                name='customerPhone'
+                data-validate='phone'
+                required
+                oninput='inputValidate(); togglePaypalButton($bLoginStatus, $nTotalPrice);'
+            />
+            <label for='account-details__mail'>Email</label>
+            <input
+                id='account-details__mail'
+                type='email'
+                name='customerEmail'
+                data-validate='email'
+                required
+                oninput='inputValidate(); togglePaypalButton($bLoginStatus, $nTotalPrice);'
+            />
+            <label for='account-details__password'
+                >Password</label
+            >
+            <input
+                id='account-details__password'
+                type='password'
+                name='customerPassword'
+                data-validate='password'
+                required
+                oninput='inputValidate(); togglePaypalButton($bLoginStatus, $nTotalPrice);'
+            />
+            <label for='account-details__confirm-password'
+                >Confirm Password</label
+            >
+            <input
+                id='account-details__confirm-password'
+                type='password'
+                name='customerPasswordConfirm'
+                data-validate='password'
+                required
+                oninput='inputValidate(); togglePaypalButton($bLoginStatus, $nTotalPrice);'
+            />
+            <h2 class='section-header'>
+                Shipping/Billing address
+            </h2>
+            <label for='account-details__street-name'
+                >Street name</label
+            >
+            <input
+                id='account-details__street-name'
+                type='text'
+                name='companyStreet'
+                data-validate='string'
+                required
+                oninput='inputValidate(); togglePaypalButton($bLoginStatus, $nTotalPrice);'
+            />
+            <label for='account-details__city'>City</label>
+            <input
+                id='account-details__city'
+                type='text'
+                data-validate='string'
+                name='companyCity'
+                required
+                oninput='inputValidate(); togglePaypalButton($bLoginStatus, $nTotalPrice);'
+            />
+            <label for='account-details__zip-code'
+                >Zip code</label
+            >
+            <input
+                id='account-details__zip-code'
+                type='text'
+                data-validate='string'
+                name='companyZip'
+                required
+                oninput='inputValidate(); togglePaypalButton($bLoginStatus, $nTotalPrice);'
+            />
+            <label for='account-details__zip-code'
+                >Country</label
+            >
+            <input
+                id='account-details__zip-code'
+                type='text'
+                data-validate='string'
+                name='companyCountry'
+                required
+                oninput='inputValidate(); togglePaypalButton($bLoginStatus, $nTotalPrice);'
+            />            
+            <div class='errorMessage'></div>
+        </form>
+        ";
+    //Return html block
+    return $sSignUpFormHtmlComp;
+}
--- cart.php
+++ cart.php
@@ -6,12 +6,13 @@ include_once("components/head.php");
 include_once("components/footer.php");
 include("db-connection/connection.php");
 include_once("components/addOn.php");
+include_once("components/signUpForm.php");
 $sHeadHtmlComp = headComp();
 $sHeaderHtmlComp = headerComp('cart');
 $sFooterHtmlComp = footerComp();
 
 //productcards being printet starts here
-$sPageHtml = "";
+$sSignUpFormHtml = "";
 $sProductHtml = "";
 $sAddonHtml = "";
 $nTotalPrice = 0;
@@ -74,144 +75,7 @@ if (isset($_SESSION['cartAddOns'])) {
 }
 //If the user is not logged in, we print the form for the user to fill out
 if ($bLoginStatus != 'true') {
-    $sPageHtml = "
-        <form action='api/payment-handler.php' method='POST' class='account-details'>
-            <h2 class='section-header'>Account details</h2>
-            <label for='account-details__name'
-                >Company Name</label
-            >
-            <input
-                id='account-details__name'
-                type='text'
-                name='companyName'
-                data-validate='string'
-                required
-                oninput='inputValidate(); togglePaypalButton($bLoginStatus, $nTotalPrice);'
-            />
-            <label for='account-details__cvr'
-                >Company CVR nr.</label
-            >
-            <input
-                id='account-details__cvr'
-                type='text'
-                name='companyCvr'
-                data-validate='cvr'
-                required
-                oninput='inputValidate(); togglePaypalButton($bLoginStatus, $nTotalPrice);'
-            />
-            <div class='account-details__contact'>
-                <h4 class='section-subheader contact__header'>Contact Person</h4>
-                <div class='contact__wrapper'>
-                    <label for='contact__firstname'>Firstname</label>
-                    <input
-                        id='contact__firstname'
-                        type='text'
-                        name='customerFirstName'
-                        data-validate='string'
-                        required
-                        oninput='inputValidate(); togglePaypalButton($bLoginStatus, $nTotalPrice);'
-                    />
-                </div>
-                <div class='contact__wrapper'>
-                    <label for='contact__lastname'>Lastname</label>
-                    <input
-                        id='contact__lastname'
-                        type='text'
-                        name='customerLastName'
-                        data-validate='string'
-                        required
-                        oninput='inputValidate(); togglePaypalButton($bLoginStatus, $nTotalPrice);'
-                    />
-                </div>
-            </div>
-            
-            <label for='account-details__mail'>Phone Number</label>
-            <input
-                id='account-details__mail'
-                type='text'
-                name='customerPhone'
-                data-validate='phone'
-                required
-                oninput='inputValidate(); togglePaypalButton($bLoginStatus, $nTotalPrice);'
-            />
-            <label for='account-details__mail'>Email</label>
-            <input
-                id='account-details__mail'
-                type='email'
-                name='customerEmail'
-                data-validate='email'
-                required
-                oninput='inputValidate(); togglePaypalButton($bLoginStatus, $nTotalPrice);'
-            />
-            <label for='account-details__password'
-                >Password</label
-            >
-            <input
-                id='account-details__password'
-                type='password'
-                name='customerPassword'
-                data-validate='password'
-                required
-                oninput='inputValidate(); togglePaypalButton($bLoginStatus, $nTotalPrice);'
-            />
-            <label for='account-details__confirm-password'
-                >Confirm Password</label
-            >
-            <input
-                id='account-details__confirm-password'
-                type='password'
-                name='customerPasswordConfirm'
-                data-validate='password'
-                required
-                oninput='inputValidate(); togglePaypalButton($bLoginStatus, $nTotalPrice);'
-            />
-            <h2 class='section-header'>
-                Shipping/Billing address
-            </h2>
-            <label for='account-details__street-name'
-                >Street name</label
-            >
-            <input
-                id='account-details__street-name'
-                type='text'
-                name='companyStreet'
-                data-validate='string'
-                required
-                oninput='inputValidate(); togglePaypalButton($bLoginStatus, $nTotalPrice);'
-            />
-            <label for='account-details__city'>City</label>
-            <input
-                id='account-details__city'
-                type='text'
-                data-validate='string'
-                name='companyCity'
-                required
-                oninput='inputValidate(); togglePaypalButton($bLoginStatus, $nTotalPrice);'
-            />
-            <label for='account-details__zip-code'
-                >Zip code</label
-            >
-            <input
-                id='account-details__zip-code'
-                type='text'
-                data-validate='string'
-                name='companyZip'
-                required
-                oninput='inputValidate(); togglePaypalButton($bLoginStatus, $nTotalPrice);'
-            />
-            <label for='account-details__zip-code'
-                >Country</label
-            >
-            <input
-                id='account-details__zip-code'
-                type='text'
-                data-validate='string'
-                name='companyCountry'
-                required
-                oninput='inputValidate(); togglePaypalButton($bLoginStatus, $nTotalPrice);'
-            />            
-            <div class='errorMessage'></div>
-        </form>";
+    $sSignUpFormHtml = signUpFormComp($bLoginStatus, $nTotalPrice);
 }
 ?>
 
@@ -227,7 +91,7 @@ if ($bLoginStatus != 'true') {
     <main id="cart" class="container-full-width">
         <section class="layout-container cart">
             <div class="checkout-container">
-                <?= $sPageHtml ?>
+                <?= $sSignUpFormHtml ?>
                 <div class="order-summary-container">
                     <h2 class="section-header">Order summary</h2>
                     <div class="order-summary">
